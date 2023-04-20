d120e1fa12136378f7f88ba17c2de8b2f6e5930a
Mercutio <mercutio_john@foxmail.com>
2023-04-20 22:39:33 +0800

    Initial commit

---
+++ src/AlbumWallSimple.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="album-cover-wall" @click="handleClickBack()">
+    <div
+      v-for="(album, index) in albums"
+      @click.stop="handleClickAlbum(index)"
+      :key="index"
+      class="album-clickbox"
+    >
+      <AlbumCover
+        :cover="album.cover"
+        :title="album.title"
+        :artist="album.artist"
+        :isFlipped="isFlipped(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AlbumCover from "./AlbumCover.vue";
+
+export default {
+  name: "AlbumCoverWall",
+  components: {
+    AlbumCover,
+  },
+  props: {
+    albums: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      flippedIndex: -1,
+    };
+  },
+  methods: {
+    handleClickAlbum(index) {
+      this.flippedIndex = index;
+    },
+    handleClickBack() {
+      this.flippedIndex = -1;
+    },
+    isFlipped(index) {
+      return this.flippedIndex === index;
+    },
+  },
+};
+</script>
+
+<style>
+.album-cover-wall {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <!-- <AlbumWall :albums="albums" /> -->
+    <AlbumWallSimple :albums="albums" />
+  </div>
+</template>
+
+<script>
+import AlbumWall from "@/AlbumWall";
+import AlbumWallSimple from "@/AlbumWallSimple";
+export default {
+  name: "App",
+  components: {
+    AlbumWall,
+    AlbumWallSimple,
+  },
+  data() {
+    return {
+      albums: [
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+        {
+          title: "专辑标题",
+          artist: "艺术家名称",
+          cover: "https://picsum.photos/200/200?random=1",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
